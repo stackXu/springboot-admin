76d64c5b1fa99d3f5fa3310c8a5963bc55bab282
yida <15268179013@139.com>
2019-12-08 17:44:59 +0800

    spring-admin

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author <a href="mailto:15268179013@139.com">yida</a>
