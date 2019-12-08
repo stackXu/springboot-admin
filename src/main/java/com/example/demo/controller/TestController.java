d9312b6be41e6e0ca26720d0a148f17964d4ee28
yida <15268179013@139.com>
2019-12-08 17:38:39 +0800

    spring-admin

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="mailto:15268179013@139.com">yida</a>
+ * @Version 2019/12/8 13:35
+ * @Version 1.0
+ * @Description TestController
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "hellow";
+    }
+
+}
